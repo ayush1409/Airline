b68fc0a224417a800046c67df6eb5b3e42c654cb
ayush1409 <ayushsingh45556@gmail.com>
2018-08-06 15:43:15 +0530

    now we can add non passengers to flight passengers

--- flights/views.py
+++ flights/views.py
@@ -1,15 +1,18 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight,Passenger
+
+
 # Create your views here.
 def index(request):
 	# pass data in form of dictionary in django
 	context = {
 		"flights" : Flight.objects.all()
 	}
-
 	return render(request, "flights/index.html", context)
+
 
 def flight(request, flight_id):
 	try:
@@ -18,7 +21,25 @@ def flight(request, flight_id):
 		raise Http404("Flight does not exist.")
 	context = {
 		"flight" : flight,
-		"passengers" : flight.Passenger.all()
+		"passengers" : flight.Passenger.all(),
+		"non_passengers" : Passenger.objects.exclude(flights=flight).all()
 	}
-
 	return render(request, "flights/flight.html", context)
+
+
+def book(request, flight_id):
+
+	try:
+		passenger_id = int(request.POST["passenger"])
+		passenger = Passenger.objects.get(pk=passenger_id)
+		flight = Flight.objects.get(pk=flight_id)
+	except KeyError :
+		return render(request, "flights/error.html", {"message" : "No selection"})
+	except Passenger.DoesNotExist :
+		return render(request, "flights/error.html", {"message" : "No passenger"})
+	except Flight.DoesNotExist :
+		return render(request, "flights/error.html", {"message" : "No flight."})
+
+	# incase of no exception -> add passenger
+	passenger.flights.add(flight)
+	return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
